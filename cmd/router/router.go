5205e535c28a8ac13bd673aefcf932b94f1c4a4b
thomastangerine <thomastang.thomas97@gmail.com>
2019-05-06 17:59:55 -0700

    no compiler errors, fix dbschema.json

--- cmd/router/router.go
+++ cmd/router/router.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net/http"
     "encoding/json"
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
+    "io/ioutil"
+    "log"
 )
 
 // Get Category Prices for Multiple Stores
@@ -12,18 +14,26 @@ func getCategoryPricesForStores(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)   // Used to get category_id from endpoint URL
 
     // Query Firebase for category_id given
-    resp, err := http.Get("https://honey4groceries.firebaseio.com/categories/" + params["category_id"] + "/storeToPrice.json")
-
-    // Parse the JSON Response
-    var storesToPrices map[string]interface{}
-    json.Unmarshal([]byte(resp), &storesToPrices)
-
-    // Close Response Body
+    resp, _ := http.Get("https://honey4groceries.firebaseio.com/categories/" + params["category_id"] + "/storeToPrice.json")
+    //if err != nil {
+      //  return err
+    //}
     defer resp.Body.Close()
+    /*
+    body, err := ioutil.ReadAll(resp.Body)
+    map := &map[string]interface{}{}
+    json.Unmarshal(body, map)
+    desiredValue := map["desiredKey"]
+    */
+    
+    // Parse the JSON Response
+    body, _ := ioutil.ReadAll(resp.Body)
+    var result map[string]interface{}
+    json.Unmarshal(body, &result)
+    //result2 := result.(map[string]interface{})
+    //storesToPrices := result[].(map[string]interface{})
 
-    // Query for Foursquare store_ids
-    foursquare := r.URL.Query()
-
+    var storePrices map[string]string
     storePrices = make(map[string]string)
 
     // Struct to Unmarshal Store Name
@@ -37,25 +47,34 @@ func getCategoryPricesForStores(w http.ResponseWriter, r *http.Request) {
      }
 
     // Loop through store_ids from Foursquare
-    for _, v := range foursquare {
+    for _, store_id := range stores["store"] {
         // Query Firebase for Store Name
-        resp, err :=
-        http.Get("https://honey4groceries.firebaseio.com/stores/" + v +
-        "/name.json")
+        resp, _ =http.Get("https://honey4groceries.firebaseio.com/stores/" + store_id + "/name.json")
+        //if err != nil {
+        //    return err
+       // }
+        defer resp.Body.Close()
 
         // Get Store name from JSON Response
         var storeName StoreName
-        json.Unmarshal([]byte(resp), &storeName)
+        json.NewDecoder(resp.Body).Decode(storeName)
+        //json.Unmarshal([]byte(resp), &storeName)
 
         // Close Response Body
         defer resp.Body.Close()
 
         // Query Firebase for Price
-        resp, err = http.Get("https://honey4groceries.firebaseio.com/prices/" + storesToPrices[v] + "/price.json")
+        
+        resp, _ = http.Get("https://honey4groceries.firebaseio.com/prices/" + result[store_id].(string) + "/price.json")
+        //if err != nil {
+          //  return err
+        //}
+        defer resp.Body.Close()
 
         // Get Price from JSON Response
         var price Price
-        json.Unmarshal([]byte(resp), &price)
+        body, _ = ioutil.ReadAll(resp.Body)
+        json.Unmarshal(body, &price)
 
         // Close Response Body
         defer resp.Body.Close()
@@ -65,6 +84,7 @@ func getCategoryPricesForStores(w http.ResponseWriter, r *http.Request) {
     }
 
     json.NewEncoder(w).Encode(storePrices)
+    //return nil
 }
 
 // Get Category Prices for a Single Store
@@ -85,15 +105,15 @@ func main() {
     categoryPrices := r.PathPrefix("/categories/{category_id}/prices").Subrouter()
 
     // Route Handlers for the categories/category_id/prices Endpoint
-    categoryPrices.Handler(getCategoryPricesForStores).Methods("GET")
-    categoryPrices.Handler(getCategoryPricesForStore).Methods("GET").Queries("store", "{store_id}")
+    categoryPrices.HandleFunc("", getCategoryPricesForStores).Methods("GET").Queries("stores", "{store_ids}")
+    categoryPrices.HandleFunc("", getCategoryPricesForStore).Methods("GET").Queries("store", "{store_id}")
 
 
     // Subrouter for the items/item_id/prices Endpoint
-    itemPrices := r.PathPrefix("/items/{item_id}/prices")
+    itemPrices := r.PathPrefix("/items/{item_id}/prices").Subrouter()
 
     // Route Handlers for the items/item_id/prices Endpoint
-    itemPrices.Handler(getItemPricesForStores).Methods("GET")
+    itemPrices.HandleFunc("", getItemPricesForStores).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
